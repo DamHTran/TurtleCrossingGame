66c115da3684567b6125a85c8783e37cb851b6c8
DamTran <thdam3012@gmail.com>
2023-03-18 19:24:03 +0200

    second commit

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from turtle import Screen
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
+TIME_SLEEP = 0.1
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -11,7 +12,7 @@ screen.tracer(0)
 
 player = Player()
 car_manager = CarManager()
-
+scoreboard = Scoreboard()
 
 # Create turtle and move
 screen.listen()
@@ -19,10 +20,21 @@ screen.onkey(player.move, "Up")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(TIME_SLEEP)
     screen.update()
-    # create a car and move it continuously
+    # create cars and move them
     car_manager.create_car()
     car_manager.move_cars()
     # Detect collision with cars
+    for car in car_manager.all_cars:
+        if player.distance(car) < 20:
+            game_is_on = False
     # Detect collision with wall
+    if player.ycor() == 280:
+        player.goto((0, -280))
+        TIME_SLEEP *= 0.8
+        scoreboard.increase_level()
+        scoreboard.update_level()
+
+
+screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,21 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("black")
+        self.penup()
+        self.hideturtle()
+        self.level = 1
+        self.update_level()
+
+    def update_level(self):
+        self.clear()
+        self.goto(-280, 250)
+        self.write(f"Level: {self.level}", font=FONT)
+
+    def increase_level(self):
+        self.level += 1
